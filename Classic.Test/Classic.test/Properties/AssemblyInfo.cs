7d27b8fa900f7e278d597a2d0a7b3e9c5f5fb746
Raznalits <91465477+Raznalits@users.noreply.github.com>
2022-07-04 10:56:50 +0300

    Add files via upload

---
+++ Classic.Test/Classic.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Classic.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Classic.test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff3ea961-4a39-4650-8706-07a4cfd9d6e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Classic.Test/Classic.test/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Classic;
+
+namespace Classic.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static Anacon AnaconClass = null;
+
+        [ClassInitialize]
+        static public void Init(TestContext tc)
+        {
+            AnaconClass = new Anacon();
+        }
+
+        
+        [TestMethod]
+        public void PopularMonths_Await3ItemsWithSortByDate()
+        {
+            List<DateTime> dates = new List<DateTime>() {
+                new DateTime(2020, 12, 17),
+                new DateTime(2020, 12, 15),
+                new DateTime(2020, 11, 17),
+                new DateTime(2020, 10, 1) };
+
+            dates = AnaconClass.PopularMonths(dates);
+
+
+            Assert.AreEqual(dates.Count, 3);
+
+
+            Assert.AreEqual(dates[0], new DateTime(2020, 12, 1));
+        }
+    }
+}
